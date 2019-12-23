6b1f17042eba5e1fa6e41fccbbf6a9004b04230d
ishworkhanal21 <ishworkhanal21@gmail.com>
2019-12-23 21:53:41 +0545

    Starting state

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var cols,rows;
+var w = 40;
+
+
+function setup() {
+  createCanvas(400, 400);
+  cols = floor(width/w);
+  rows = floor(height/w);
+}
+
+function draw() {
+  background(51);
+}
+
+function cell(i,j){
+    this.i = i;
+    this.j = j;
+}
